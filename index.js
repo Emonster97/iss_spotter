0633d56395dcec4108309ad6ba09f835bd88db1a
Wilfred <wilfredemonts@gmail.com>
2022-01-07 01:52:09 -0500

    remade the iss spotter using promises, the code is much easier to understand and more efficient

--- index.js
+++ index.js
@@ -2,15 +2,8 @@
 
 // The code below is temporary and can be commented out.
 const { nextISSTimesForMyLocation } = require('./iss');
+const {printPassTimes} = require('./iss_promised');
 
-const printPassTimes = function(passTimes) {
-  for (const pass of passTimes) {
-    const datetime = new Date(0);
-    datetime.setUTCSeconds(pass.risetime);
-    const duration = pass.duration;
-    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
-  }
-};
 
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
